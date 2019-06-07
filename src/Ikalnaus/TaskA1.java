819bff68d023bbffc8a1b99704cca9ee77b62954
IvanKalnaus <ivan.kalnaus@mail.ru>
2019-06-07 17:11:30 +0300

    no message

---
+++ src/Ikalnaus/TaskA1.java
@@ -0,0 +1,9 @@
+public class TaskA1 {
+    public static void main (String[] args){
+        int number=1000;
+        for (int i=0; number<10000; i++){
+            System.out.println(number);
+            number+=3;
+        }
+    }
+}
---
+++ src/Ikalnaus/TaskA3.java
@@ -0,0 +1,9 @@
+public class TaskA3 {
+    public static void main (String[] args){
+        int num=90;
+        for(int i=0; num >= 0; i++){
+            System.out.println(num);
+            num -= 5;
+        }
+    }
+}
